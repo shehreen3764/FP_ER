8984faa20ad6ebdbf448949e8de025ab36e64230
Shehreen <shehreenazad@iut-dhaka.edu>
2019-03-20 23:49:27 +0600

    another change

---
+++ Source/FP_ER/CornerTiles.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseTile.h"
+#include "CornerTiles.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FP_ER_API ACornerTiles : public ABaseTile
+{
+	GENERATED_BODY()
+	
+};
